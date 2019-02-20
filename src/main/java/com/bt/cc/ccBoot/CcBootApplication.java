ec2ced6fb32f0106a2c6be817283ce37b980c7e4
Lenin Rajavel <l3ninrajav3l@hotmail.com>
2019-02-19 21:15:47 -0500

    Bootstrapping the project

---
+++ src/main/java/com/bt/cc/ccBoot/CcBootApplication.java
@@ -0,0 +1,13 @@
+package com.bt.cc.ccBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcBootApplication.class, args);
+	}
+
+}
